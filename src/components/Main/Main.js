8b298a8a504b29fd6b97d46fd45065f118db6a12
Andrey Zverev <a.zverev@severnaya.ru>
2024-01-02 22:45:59 +0300

    добавил Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import Promo from "../Promo/Promo";
 import Header from "../Header/Header"
 import AboutProject from "../AboutProject/AboutProject"
+import Techs from "../Techs/Techs";
 function Main (){
     return (
         <div className="App">
             <Header />
             <Promo />
             <AboutProject />
+            <Techs />
         </div>
     );
 }
